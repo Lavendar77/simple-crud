46b6622a3f66616c2008de93534b47238403096d
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-09-17 17:52:34 +0100

    WIP: Complete authentication processes (Register & Login)
    WIP:
    - Encrypt password
    - Generate a user token

---
+++ src/Controller/Api/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    private UserRepository $userRepository;
+    private EntityManagerInterface $entityManagerInterface;
+
+    /**
+     * @param UserRepository $userRepository
+     * @param EntityManagerInterface $entityManagerInterface
+     */
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManagerInterface)
+    {
+        $this->userRepository = $userRepository;
+        $this->entityManagerInterface = $entityManagerInterface;
+    }
+
+    /**
+     * Create an account.
+     * 
+     * @Route("/api/register", name="api_register", methods={"POST"})
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user
+            ->setFirstName($request->get('first_name'))
+            ->setLastName($request->get('last_name'))
+            ->setUsername($request->get('username'))
+            ->setPassword($request->get('password')) // WIP: Encrypt password
+        ;
+
+        $this->entityManagerInterface->persist($user);
+        $this->entityManagerInterface->flush();
+
+        return new JsonResponse([
+        	'message' => 'Registration successful.',
+        	'token' => '{{token}}'
+        ]);
+    }
+
+    /**
+     * Sign-in to the system.
+     * 
+     * @Route("/api/login", name="api_login", methods={"POST"})
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $username = $request->get('username');
+        $password = $request->get('password');
+
+        $user = $this->userRepository->findByUsernameAndPassword($username, $password);
+
+        if (!$user) {
+            return new JsonResponse([
+                'username' => 'The provided credentials are incorrect.'
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return new JsonResponse([
+        	'message' => 'Login successful.',
+        	'data' => [
+                'user' => $user,
+                'token' => '{{token}}'
+            ]
+        ]);
+    }
+}
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -19,22 +19,21 @@ class UserRepository extends ServiceEntityRepository
         parent::__construct($registry, User::class);
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return User[] Returns an array of User objects
+     */
+    public function findByUsernameAndPassword($username, $password)
     {
         return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
+            ->andWhere('u.username = :username')
+            ->setParameter('username', $username)
+            ->andWhere('u.password = :password')
+            ->setParameter('password', $password)
             ->getQuery()
-            ->getResult()
+            ->getOneOrNullResult()
         ;
     }
-    */
+    
 
     /*
     public function findOneBySomeField($value): ?User
