eb34fdc97a27089f072872986fa625e27b0e7f34
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-10-11 15:38:40 +0100

    First test to see how it works 🤨

---
+++ tests/Api/Auth/RegisterUserTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Api\Auth;
+
+use PHPUnit\Framework\TestCase;
+
+class RegisterUserTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
