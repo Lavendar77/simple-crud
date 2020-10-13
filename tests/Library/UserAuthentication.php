b3912aeab790ca807db96a9e172bf682ba6c9346
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-10-13 13:53:49 +0100

    Create UserAuthentication.php
    Authenticate a user with this class to reduce code redundancy

---
+++ tests/Library/UserAuthentication.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Tests\Library;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserAuthentication extends WebTestCase
+{
+	/**
+	 * Create a client with a default Authorization header.
+	 *
+	 * @param string $username
+	 * @param string $password
+	 * @return \Symfony\Bundle\FrameworkBundle\Client
+	 */
+	public static function createAuthenticatedUser(string $username, string $password = 'password')
+	{
+		$client = static::createClient();
+
+        $crawler = $client->request(
+        	'POST',
+        	'/api/login',
+        	[],
+	        [],
+	        [
+				'CONTENT_TYPE' => 'application/json'
+			],
+			json_encode([
+	        	'username' => $username,
+	        	'password' => $password,
+	        ])
+	    );
+
+	    return $client;
+	}
+}
