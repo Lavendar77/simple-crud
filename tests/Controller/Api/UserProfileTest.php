0468a12d273b3c92684c80b55746e522f145f201
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-10-13 13:54:04 +0100

    Create UserProfileTest.php

---
+++ tests/Controller/Api/UserProfileTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Controller\Api;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use App\Tests\Library\UserAuthentication;
+
+class UserProfileTest extends WebTestCase
+{
+	/**
+     * Test an authenticated access to a user profile.
+     * 
+     * @return void
+     */
+    public function testSuccessfulAccessToProfile()
+    {
+        $client = UserAuthentication::createAuthenticatedUser('test@example.com', 'password');
+        $data = json_decode($client->getResponse()->getContent());
+
+       	$client->setServerParameter('HTTP_Authorization', sprintf('Bearer %s', $data->token));
+
+        $crawler = $client->request(
+        	'GET',
+        	'/api/user',
+        	[],
+        	[],
+        	[
+        		'CONTENT_TYPE' => 'application/json',
+        	]
+       	);
+
+        // Assert that the response status code is 200 (HTTP_OK)
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        // Assert that the "Content-Type" header is "application/json"
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
+    }
+
+    /**
+     * Test a unauthenticated access to a user profile.
+     * 
+     * @return void
+     */
+    public function testFailedAccessToProfile()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/api/user');
+
+        // Assert that the response status code is 401 (HTTP_UNAUTHORIZED)
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNAUTHORIZED);
+    }
+}
